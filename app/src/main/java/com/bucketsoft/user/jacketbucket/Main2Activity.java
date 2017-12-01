0c62b63241a0d0aad36c47e2fd3d00a1ec8c135d
gürkan soykan <gsoykan@sabanciuniv.edu>
2017-12-01 14:06:57 +0300

    yeni commit

--- app/src/main/java/com/bucketsoft/user/jacketbucket/Main2Activity.java
+++ app/src/main/java/com/bucketsoft/user/jacketbucket/Main2Activity.java
@@ -2,12 +2,28 @@ package com.bucketsoft.user.jacketbucket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+
+    private Button mBtGoBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        mBtGoBack = (Button) findViewById(R.id.button2);
+
+        mBtGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
+
     }
 }
